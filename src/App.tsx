3076dac80d3e95ab77376029af9698e4602fb022
Sil0 <silo@udel.edu>
2024-02-16 16:18:31 -0500

    Added two column formatting and a list with three things that I like to my web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import "./App.css";
-
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Silo Murphy UD CISC275 with React Hooks and TypeScript
+                Silo Murphy UD CISC275 with React Hooks and TypeScript. This is
+                my page header.
             </header>
-            <p>Hello World</p>
+            <p>
+                Hello World. This is my webpage.
+                <Container>
+                    <Row>
+                        <Col>
+                            Here are three things I like:
+                            <ul>
+                                <li>math</li>
+                                <li>singing</li>
+                                <li>rock climbing</li>
+                            </ul>
+                        </Col>
+                        <Col>Here is a nice rectangle.</Col>
+                    </Row>
+                </Container>
+            </p>
         </div>
     );
 }
