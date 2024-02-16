c8461e704de9826fd42b1fad0556530f64f24e4a
Sil0 <silo@udel.edu>
2024-02-16 15:07:57 -0500

    troubleshooting merge

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Silo Murphy UD CISC275 with React Hooks and TypeScript. mit Hello World.
+                Silo Murphy UD CISC275 with React Hooks and TypeScript. Hello World.
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
