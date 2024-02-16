9c72f88e372e4cbc013db64310b7a123ace846c5
Sil0 <silo@udel.edu>
2024-02-16 15:25:36 -0500

    still troubleshooting

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Silo Murphy UD CISC275 with React Hooks and TypeScript. Hello World.
+                Silo Murphy UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <p>Hello World</p>
         </div>
     );
 }
