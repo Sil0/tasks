bde3071fea21ac31a7e465915d489f17978aba83
Sil0 <silo@udel.edu>
2024-02-18 14:55:43 -0500

    added tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripledNumbers = numbers.map((num: number): number => 3 * num);
+    return tripledNumbers;
 }
 
 /**
