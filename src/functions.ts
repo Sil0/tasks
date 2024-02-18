94dfc1f7602321de8db9b0c17a56a9031394cd82
Sil0 <silo@udel.edu>
2024-02-18 14:33:55 -0500

    added rest of functions.

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,11 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return first + second + third;
+    return Math.abs(
+        ((1 + Math.sign(first)) * first) / 2 +
+            ((1 + Math.sign(second)) * second) / 2 +
+            ((1 + Math.sign(third)) * third) / 2
+    );
 }
 
 /**
@@ -20,7 +24,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +32,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.slice(-1) === "?";
 }
 
 /**
@@ -37,5 +41,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    } else if (word === "no" || word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
