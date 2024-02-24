f9886f922bc5d9286d6eb14206900ae4f6f5d8e8
Sil0 <silo@udel.edu>
2024-02-24 15:20:47 -0500

    implemented the getPublishedQuestions function

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,10 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    const publishedQuestions = questions.filter(
+        (qst: question): question => qst.published
+    );
+    return publishedQuestions;
 }
 
 /**
