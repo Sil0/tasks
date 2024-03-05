cf1f064eda9aa5cc5d5e993573c356b9eed0405d
Sil0 <silo@udel.edu>
2024-03-04 20:48:21 -0500

    added revealAnswer component

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,11 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isHidden, setHidden] = useState<boolean>(true);
+    return (
+        <span>
+            <Button onClick={() => setHidden(!isHidden)}>Reveal Answer</Button>
+            {!isHidden && <p>42</p>}
+        </span>
+    );
 }
