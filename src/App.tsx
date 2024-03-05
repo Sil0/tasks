50b7d1953154c6d1cd0d67b820cc430ff13cdbe2
Sil0 <silo@udel.edu>
2024-03-04 20:33:35 -0500

    fixed merge errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Container } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
@@ -20,50 +13,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 Silo Murphy UD CISC275 with React Hooks and TypeScript.
             </header>
-<<<<<<< HEAD
-            <h1>This is my page header.</h1>
-            <p>Hello World. This is my webpage.</p>
-            <Container>
-                <Row>
-                    <Col>
-                        Here are three things I like:
-                        <ol>
-                            <li>math</li>
-                            <li>rock climbing</li>
-                            <li>singing</li>
-                        </ol>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                        <div
-                            style={{
-                                border: "1px solid red",
-                                padding: "4px",
-                                width: "500px",
-                                height: "100px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                    <Col>
-                        Here is a cool image.
-                        <img
-                            src={require("./images/cool_dinosaur.jpeg")}
-                            alt="A cool picture of a dinosaur"
-                        />
-                        <div
-                            style={{
-                                border: "1px solid red",
-                                padding: "4px",
-                                width: "50px",
-                                height: "10px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -76,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
