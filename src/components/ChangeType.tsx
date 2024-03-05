ef8291922c939a7bcf8f041b275cad9ffa46d015
Sil0 <silo@udel.edu>
2024-03-05 13:54:30 -0500

    added changeType but it is not yet finished

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,26 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    const swapType = () => {
+        setQuestionType(
+            questionType === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+        console.log(questionType);
+    };
+
+    return (
+        <span>
+            <Button onClick={swapType}>Change Question Type</Button>
+            <p>
+                {questionType === "multiple_choice_question"
+                    ? "Multiple Choice Question"
+                    : "Short Answer Question"}
+            </p>
+        </span>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,5 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    return <div>Start attempt</div>;
 }
