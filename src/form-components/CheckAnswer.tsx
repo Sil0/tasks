2f113088ce0fd0f3b921a16946fc87684e72b36c
Sil0 <silo@udel.edu>
2024-03-07 17:59:48 -0500

    troubleshooting merge

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
+import exp from "constants";
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <div>
+                <Form.Group controlId="formName">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control value={answer} onChange={updateAnswer} />
+                </Form.Group>
+            </div>
+            <div>{answer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
